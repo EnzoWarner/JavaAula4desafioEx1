fee57c78fdee3a50d8b81596f1ae3ce2101724ea
EnzoWarner <enzo_warner@hotmail.com>
2024-05-21 02:29:03 -0300

    first commit

---
+++ Ex_4_1Desafio.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+public class Ex_4_1Desafio {
+public static void main (String[] args){
+String valor1 = JOptionPane.showInputDialog(null, "Insira o 1º Número: ");
+int ivalor1 = Integer.parseInt(ivalor1);
+     String valor2 = JOptionPane.showInputDialog(null, " Insira o 2º Número: ");
+      int ivalor2 = Integer.parseInt(ivalor2);
+      int R = (ivalor1 + ivalor2);
+      JOptionPane.showMessageDialog(null, " A soma é: " +R);
+}
+}
